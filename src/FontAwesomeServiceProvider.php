4ad8c4154f6a0e18d84e0a03b765f279db1b0c07
Grant Pearce <grant@focalstrategy.com>
2020-09-19 07:02:33 +0100

    add js for fontawesome

--- src/FontAwesomeServiceProvider.php
+++ src/FontAwesomeServiceProvider.php
@@ -17,7 +17,7 @@ class FontAwesomeServiceProvider extends ServiceProvider
         $this->loadViewsFrom(__DIR__ . '/Views', 'font_awesome');
         $this->publishes([__DIR__ . '/Config/font_awesome.php' => config_path('font_awesome.php')], 'config');
         // $this->publishes([__DIR__ . '/Views/publish' => app_path('Vendor/FontAwesome')]);
-        // $this->publishes([__DIR__ . '/Public' => public_path('vendor/loopy/font_awesome')], 'public');
+        $this->publishes([__DIR__ . '/Public' => public_path('vendor/loopy/font_awesome')], 'public');
     }
 
     /**
