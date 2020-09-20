d4cb33118822e4ddd473bd3d59d92df450c18ace
Grant Pearce <grant@focalstrategy.com>
2020-09-20 06:56:22 +0100

    refactor

--- src/Services/FontAwesomeManager.php
+++ src/Services/FontAwesomeManager.php
@@ -14,36 +14,29 @@ class FontAwesomeManager
 
     public function __call(string $name, array $data = []) : string
     {
+        $this->frame = null;
         $this->name = $this->camel2dashed($name);
         $this->checkFrame();
+        $this->setData($data);
 
-        if (is_array($data) && isset($data[0])) {
-            if (!is_string($data[0])&& !is_integer($data[0]) || count($data) > 1) {
-                foreach ($data[0] as $key => $value) {
-                    $key = snake_case($key);
-                    $this->$key = $value;
-                }
-            } elseif (is_string($data[0])) {
-                $this->class = $data[0];
-            } elseif (is_integer($data[0])) {
-                $this->count = $data[0];
-            }
-        }
-        $default_icon = view()->exists('font_awesome::publish.' . snake_case($name));
-        if ($this->frame) {
+        $blade_template = snake_case($name);
+        $default_icon = view()->exists('font_awesome::publish.' . $blade_template);
+        $type = $default_icon ? '::publish.' : '.';
+
+        if ($this->frame && view()->exists('font_awesome::' . $this->frame)) {
             return view('font_awesome::' . $this->frame)
             ->with('item', $this)
             ->render();
-        }
-
-        if (!$default_icon && !view()->exists('font_awesome.' . snake_case($name))) {
+        } elseif ($this->frame && view()->exists('font_awesome.' . $this->frame)) {
+            return view('font_awesome.' . $this->frame)
+            ->with('item', $this)
+            ->render();
+        } elseif (!$default_icon && !view()->exists('font_awesome.' . $blade_template)) {
             return view('font_awesome::default')
             ->with('item', $this)
             ->render();
         }
-
-        $type = $default_icon ? '::publish.' : '.';
-        return view('font_awesome' . $type . snake_case($name))
+        return view('font_awesome' . $type . $blade_template)
         ->with('item', $this)
         ->render();
     }
@@ -96,6 +89,24 @@ class FontAwesomeManager
             unset($box[1]);
             unset($box[0]);
             $this->name = implode('-', $box);
+        }
+    }
+
+    private function setData(array $data)
+    {
+        if (!empty($data[0])) {
+            if (count($data) == 1) {
+                if (is_string($data[0])) {
+                    $this->class = $data[0];
+                } elseif (is_integer($data[0])) {
+                    $this->count = $data[0];
+                }
+            } elseif (count($data) > 1) {
+                foreach ($data[0] as $key => $value) {
+                    $key = snake_case($key);
+                    $this->$key = $value;
+                }
+            }
         }
     }
 }
