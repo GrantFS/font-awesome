551a2c23e5621f7ac33f1859322d4f11ed60f504
Grant Pearce <grant@focalstrategy.com>
2020-09-20 06:15:35 +0100

    remove test number

--- src/Views/publish/comments.blade.php
+++ src/Views/publish/comments.blade.php
@@ -1,6 +1,6 @@
 <span class="fa-layers fa-fw fa-lg">
 	<span class="fas fa-comment" data-fa-transform="grow-5"></span>
 	<span class="fa-layers-text text-dark" data-fa-transform="shrink-7" style="font-weight:700;color:white">
-		{{ str_limit($item->getCount() > 100 ? 999 : $item->getCount() ,2, '+')}}1
+		{{ str_limit($item->getCount() > 100 ? 999 : $item->getCount() ,2, '+')}}
 	</span>
 </span>
